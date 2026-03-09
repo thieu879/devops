1c1c2a373c5d2f5dfccde879163330f8a4710d0b
thieunguyen879 <nguyenthieu11022005@gmail.com>
2026-03-09 14:30:52 +0700

    first commit

---
+++ src/main/java/com/data/hn_ks23_nguyengiathieu_002/HnKs23NguyenGiaThieu002Application.java
@@ -0,0 +1,13 @@
+package com.data.hn_ks23_nguyengiathieu_002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnKs23NguyenGiaThieu002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnKs23NguyenGiaThieu002Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/hn_ks23_nguyengiathieu_002/HnKs23NguyenGiaThieu002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.hn_ks23_nguyengiathieu_002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnKs23NguyenGiaThieu002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
